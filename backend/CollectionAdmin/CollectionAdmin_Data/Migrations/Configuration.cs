4bc87b23cea2b2f4325e065153e2c3be83615da0
Maarten Jakobs <maarten.jakobs@gmail.com>
2020-10-09 13:36:25 +0200

    admin-backend init
    admin backend opgezet met product entity

---
+++ backend/CollectionAdmin/CollectionAdmin_Data/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace CollectionAdmin_Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CollectionAdmin_Data.CollectionAdminContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "CollectionAdmin_Data.CollectionAdminContext";
+        }
+
+        protected override void Seed(CollectionAdmin_Data.CollectionAdminContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
